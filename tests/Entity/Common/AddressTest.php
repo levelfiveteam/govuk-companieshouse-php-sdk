ab0d40d3f576eb079d4d463a774abe77c5d01d07
Gaurav Malhotra <gaurav@level5.co.uk>
2020-01-24 20:52:48 +0000

    Address entity test

---
+++ tests/Entity/Common/AddressTest.php
@@ -0,0 +1,35 @@
+<?php
+namespace Test\Entity\Common;
+
+use LevelFive\CompaniesHouse\Entity\Common\Address;
+use PHPStan\Testing\TestCase;
+
+class AddressTest extends TestCase
+{
+    public function testAddress()
+    {
+        $response = [
+            'address_line_1' => '10 Downing Street',
+            'address_line_2' => 'London',
+            'locality' => 'London.',
+            'region' => 'Greater London',
+            'postal_code' => 'SW1 3WQ',
+            'country' => 'United Kingdom',
+            'po_box' => 'POBOX 1003',
+            'premises' => 'Building',
+            'care_of' => 'test',
+        ];
+
+        $address = new Address($response);
+
+        self::assertEquals($response['address_line_1'], $address->getFirstLine());
+        self::assertEquals($response['address_line_2'], $address->getSecondLine());
+        self::assertEquals($response['locality'], $address->getLocality());
+        self::assertEquals($response['region'], $address->getRegion());
+        self::assertEquals($response['postal_code'], $address->getPostalCode());
+        self::assertEquals($response['country'], $address->getCountry());
+        self::assertEquals($response['po_box'], $address->getPoBox());
+        self::assertEquals($response['premises'], $address->getPremises());
+        self::assertEquals($response['care_of'], $address->getCareOf());
+    }
+}
